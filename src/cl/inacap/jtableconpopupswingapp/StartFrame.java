9840f81c9161138bc5ee8e0eed6f0c3c9e36e784
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-09 17:20:21 -0300

    Ok

--- src/cl/inacap/jtableconpopupswingapp/StartFrame.java
+++ src/cl/inacap/jtableconpopupswingapp/StartFrame.java
@@ -51,19 +51,19 @@ public class StartFrame extends JFrame {
 		table = new JTable();
 		table.setBounds(10, 11, 614, 339);
 		contentPane.add(table);
-		popUpTable(); //Finalmente se llama al m�todo aqu� (De esta forma queda reconocido el PopUp).
+		popUpTable(); //Finalmente se llama al método aquí (De esta forma queda reconocido el PopUp).
 	}
 	
 	public void popUpTable(){
 		JPopupMenu pm = new JPopupMenu(); //Aca se crea el PopUp (La ventana).
-		JMenuItem mi = new JMenuItem("Agregar", new ImageIcon("Imagenes/add.png")); //Aca se crea un JMenuItem (Para luego a�adirlo al PopUp).
+		JMenuItem mi = new JMenuItem("Agregar", new ImageIcon("Imagenes/add.png")); //Aca se crea un JMenuItem (Para luego añadirlo al PopUp).
 		mi.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) { //Aca va el c�digo que se ejecuta cuando se selecciona este JMenuItem (Ac� se podr�a tomar los datos de la celda seleccinada y realizar una acci�n).
+			public void actionPerformed(ActionEvent e) { //Aca va el código que se ejecuta cuando se selecciona este JMenuItem (Acá se podría tomar los datos de la celda seleccinada y realizar una acción).
 				JOptionPane.showMessageDialog(null, "Seleccionaste el mensaje");	
 			}
 		});
-		pm.add(mi); //Aca se le a�ade el JMenuItem al PopUp.
-		table.setComponentPopupMenu(pm); //Aca se le a�ade el PopUp al JTable.
+		pm.add(mi); //Aca se le añade el JMenuItem al PopUp.
+		table.setComponentPopupMenu(pm); //Aca se le añade el PopUp al JTable.
 	}
 }
